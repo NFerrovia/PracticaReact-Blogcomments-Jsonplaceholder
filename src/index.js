348c72a16f62446a05902fc1d3289e2722fa41b0
NFerrovia <73561281+NFerrovia@users.noreply.github.com>
2021-01-15 03:31:51 -0300

    Comentarios en Código y README

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import App from "./components/App";
-import reducers from "./reducers";
+// En la siguiente linea se encuentra el Import del Middleware de esta app,
+// Thunk, que nos ayuda a lidiar con acciones asincrónicas.
+
+import thunk from 'redux-thunk';
+
+import App from './components/App';
+import reducers from './reducers';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
@@ -13,5 +17,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.querySelector("#root")
+  document.querySelector('#root')
 );
